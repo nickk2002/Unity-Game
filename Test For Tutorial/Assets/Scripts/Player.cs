4eb8a247e4981a3d52ed96c15df1a177b55915b6
nickk2002 <nicolae.filat@yahoo.com>
2019-05-18 13:05:18 +0300

    push

--- Test For Tutorial/Assets/Scripts/Player.cs
+++ Test For Tutorial/Assets/Scripts/Player.cs
@@ -77,7 +77,7 @@ public class Player : MonoBehaviour
     private void ToggleInventory()
     {
         
-        if (Input.GetKeyDown(KeyCode.I))
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
             if (allInventory.gameObject.active)
             {
